0a992acafde138f6a593c02bb354ec4670bec606
Oana Madalina Fron <o.m.fron-1@student.tudelft.nl>
2023-02-21 10:17:41 +0100

    Logic Gates problem

--- main.py
+++ main.py
@@ -4,12 +4,28 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
 from sklearn.model_selection import train_test_split
+import NeuralNetwork as NN
 
 def train_test(name):
-    X = np.loadtxt("features.txt", dtype = 'i', delimiter = ',')
-    Y = np.loadtxt("targets.txt", dtype = 'i')
+    #X = np.loadtxt("features.txt", dtype = 'i', delimiter = ',')
+    #Y = np.loadtxt("targets.txt", dtype = 'i')
+    # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2)
+    X = [[0, 0],
+         [0, 1],
+         [1, 0],
+         [1, 1]]
+
+    Y_and = [0, 0, 0, 1]
+    Y_or = [0, 1, 1, 1]
+    Y_xor = [0, 1, 1, 0]
+
+    nn_structure = [
+        {"input_dim": 2, "output_dim": 1, "activation": "step"}
+    ]
+
+    neural_network = NN(nn_structure, 0.1)
+    neural_network.update_weights(X, Y_and)
 
 
 
